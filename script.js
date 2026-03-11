bd65d25edeb25c154916e124ea3f0936988fe8c0
Oricher <ivompb2000@gmail.com>
2026-03-11 19:31:38 -0300

    Link tree Contact-Light concluida

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Script para alternar entre os temas claro e escuro
 const inputs = document.querySelectorAll('input[name="theme"]');
 inputs.forEach(input => {
     input.addEventListener('change', () => {
@@ -8,3 +9,28 @@ inputs.forEach(input => {
         }
     });
 });
+
+// Função para mostrar/esconder o contato
+function toggleContact() {
+    const overlay = document.getElementById('contact-overlay');
+    
+    // Se estiver escondido, mostra. Se estiver visível, esconde.
+    if (overlay.style.display === 'flex') {
+        overlay.style.display = 'none';
+    } else {
+        overlay.style.display = 'flex';
+    }
+}
+
+// Opcional: Fechar se clicar no fundo escuro (fora da caixinha)
+window.onclick = function(event) {
+    const overlay = document.getElementById('contact-overlay');
+    if (event.target == overlay) {
+        overlay.style.display = 'none';
+    }
+}
+
+function copyEmail() {
+    navigator.clipboard.writeText("ivompb2000@gmail.com");
+    alert("E-mail copiado com sucesso!");
+}
